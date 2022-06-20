c62942ef234ea40fa6f3af10197bb8909774c8ce
OmarJabri7 <omarjabri967@gmail.com>
2022-06-20 20:20:59 +0100

    Initial version of euro med

---
+++ doc_analysis.py
@@ -0,0 +1,32 @@
+import cv2
+import pytesseract as pt
+import utils.image_functions as img
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    form = cv2.imread('data/form.jpeg')
+    form_gray = img.get_grayscale(form)
+
+    blur = cv2.GaussianBlur(form_gray, (7, 7), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Create rectangular structuring element and dilate
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    dilate = cv2.dilate(thresh, kernel, iterations=4)
+
+    # Find contours and draw rectangle
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    all_texts = pd.DataFrame([], columns=["Sections"])
+    for c in cnts:
+        x, y, w, h = cv2.boundingRect(c)
+        sub_img = form[x:x + h, x:x + w]
+        text = pt.image_to_string(sub_img)
+        text_dat = pd.DataFrame(text.split("\n\n"))
+        all_texts = all_texts.append(pd.DataFrame(np.array(text_dat, dtype=str)))
+        img_dat = pt.image_to_data(sub_img)
+        cv2.rectangle(form, (x, y), (x + w, y + h), (36, 255, 12), 2)
+    all_texts.to_csv('output/form.csv', index=False)
+    cv2.imwrite('output/form.png', form)
+
